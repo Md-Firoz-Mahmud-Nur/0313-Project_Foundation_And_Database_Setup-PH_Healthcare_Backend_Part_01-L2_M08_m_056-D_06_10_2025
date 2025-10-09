cb36f757bc64725fa04c8d07297cc211b582d32d
firoz.nur <firoznur5@gmail.com>
2025-10-09 06:35:34 +0600

    Add : Uptime

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import config from "./config";
 
 const app: Application = express();
 app.use(
@@ -17,7 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
-    Message: "Ph health care server..",
+    message: "Server is running..",
+    environment: config.node_env,
+    uptime: process.uptime().toFixed(2) + " sec",
+    timeStamp: new Date().toISOString(),
   });
 });
 
